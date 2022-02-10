ee61ac80c35b42185f83ff2374a3c114cb7b38fd
Lzq <798945742@qq.com>
2022-02-10 22:50:19 +0800

    [Update] 完成283

--- leetcode/283. 移动零/main.py
+++ leetcode/283. 移动零/main.py
@@ -1,12 +1,14 @@
-
+import unittest
 from typing import List
 
+
 class Solution:
+
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        fast = slow =0
+        fast = slow = 0
         while fast < len(nums):
             if nums[fast] != 0 and nums[slow] == 0:
                 nums[slow], nums[fast] = nums[fast], nums[slow]
@@ -18,10 +20,62 @@ class Solution:
             if fast == slow:
                 fast += 1
                 # 防止数组越界
-                continue 
+                continue
 
             if nums[fast] == 0:
                 fast += 1
             if nums[slow] != 0:
                 slow += 1
-            
+
+    def moveZeroes_leetcode(self, nums: List[int]) -> None:
+        n = len(nums)
+        fast = slow = 0
+        while fast < n:
+            if nums[fast] != 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow += 1
+            fast += 1
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_moveZeroes(self):
+        func = Solution().moveZeroes
+
+        testnums = [
+            # 正常例子-示例
+            {
+                "nums": [0, 1, 0, 3, 12],
+                "want": [1, 3, 12, 0, 0]
+            },
+            # 特殊例子
+            {
+                "nums": [0, 0, 0],
+                "want": [0, 0, 0]
+            },
+            {
+                "nums": [1, 2, 3],
+                "want": [1, 2, 3]
+            },
+            {
+                "nums": [],
+                "want": []
+            },
+            # 边界测试
+            {
+                "nums": [0, 1, 2, 3, 0],
+                "want": [1, 2, 3, 0, 0]
+            },
+            {
+                "nums": [1, 0, 0, 0, 2],
+                "want": [1, 2, 0, 0, 0]
+            }
+        ]
+
+        for item in testnums:
+            func(item["nums"])
+            self.assertListEqual(item["nums"], item["want"])
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ leetcode/283. 移动零/main_test.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func moveZeroes(nums []int) {
+	// nums 为 切片, 为引用类型
+	var n int = len(nums)
+	var fast, slow int = 0, 0
+	for fast < n {
+		if nums[fast] != 0 && nums[slow] == 0 {
+			nums[slow], nums[fast] = nums[fast], nums[slow]
+			fast++
+			slow++
+			continue
+		}
+		if fast == slow {
+			fast++
+			continue
+		}
+		if nums[fast] == 0 {
+			fast++
+		}
+		if nums[slow] != 0 {
+			slow++
+		}
+	}
+}
+
+// func moveZeroes_leetcode(nums []int) {
+// 	var n int = len(nums)
+// 	var fast, slow int = 0, 0
+// 	for fast < n {
+// 		if nums[fast] != 0 {
+// 			nums[slow], nums[fast] = nums[fast], nums[slow]
+// 			slow++
+// 		}
+// 		fast++
+// 	}
+// }
+
+func TestMoveZeroes(t *testing.T) {
+	var tests = []struct {
+		nums []int
+		want []int
+	}{
+		//正常例子
+		{[]int{0, 1, 0, 3, 12}, []int{1, 3, 12, 0, 0}},
+		//特殊例子
+		{[]int{0, 0, 0}, []int{0, 0, 0}},
+		{[]int{1, 2, 3}, []int{1, 2, 3}},
+		{[]int{}, []int{}},
+		//边界例子
+		{[]int{0, 1, 2, 3, 0}, []int{1, 2, 3, 0, 0}},
+		{[]int{1, 0, 0, 0, 2}, []int{1, 2, 0, 0, 0}},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%v", tt.nums)
+		t.Run(testname, func(t *testing.T) {
+			moveZeroes(tt.nums)
+			if !reflect.DeepEqual(tt.nums, tt.want) {
+				t.Errorf("got %v, want %v", tt.nums, tt.want)
+			}
+		})
+	}
+}
