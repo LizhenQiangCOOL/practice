b3fd935531881a757d1b5dcabeabf0134e7298b8
lzq <798945742@qq.com>
2022-12-28 22:29:42 +0800

    [Update] 运行代码保存

--- practice-go/go-zero-example/go-zero-demo/greet/greet.go
+++ practice-go/go-zero-example/go-zero-demo/greet/greet.go
@@ -15,17 +15,22 @@ import (
 var configFile = flag.String("f", "etc/greet-api.yaml", "the config file")
 
 func main() {
+	//解析命令
 	flag.Parse()
 
+	// 读取并映射配置文件到 config 结果体
 	var c config.Config
 	conf.MustLoad(*configFile, &c)
 
+	// 初始化服务
 	server := rest.MustNewServer(c.RestConf)
 	defer server.Stop()
 
+	// 挂在 处理程序
 	ctx := svc.NewServiceContext(c)
 	handler.RegisterHandlers(server, ctx)
 
+	// 启动服务
 	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
 	server.Start()
 }
