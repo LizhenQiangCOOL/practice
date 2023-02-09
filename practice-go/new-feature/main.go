4cfc8b836f960f5e60fcef501c493f4bdb9f58d3
lzq <798945742@qq.com>
2023-02-09 22:49:15 +0800

    [Update] 运行代码保存

--- practice-go/new-feature/main.go
+++ practice-go/new-feature/main.go
@@ -1,7 +1,18 @@
 package main
 
-const aa = "aa"
+import "fmt"
+
+func f(slice []int) []int {
+	if len(slice) > 2 {
+		slice[2] = 100
+	}
+	return slice
+}
 
 func main() {
-
+	s := make([]int, 3, 5)
+	s[0], s[1], s[2] = 0, 1, 2
+	s1 := f(s)
+	fmt.Printf("s: %+v \n", s)
+	fmt.Printf("s1:%+v \n", s1)
 }
---
+++ practice-python/algorithms/main.py
@@ -0,0 +1,29 @@
+
+def main():
+    a = [0 for _ in range(9)]
+    an = True
+    for i  in range(123,333):
+        k1 = 2*i
+        k2 = 3*i
+        a[0]=i%10
+        a[1]=i//10%10
+        a[2]=i//100
+        a[3]=k1%10
+        a[4]=k1//10%1
+        a[5]=k1//100
+        a[6]=k2%10
+        a[7]=k2//10%10
+        a[8]=k2//100
+        a.sort()
+        for j in range(9):
+            if a[j]!=j+1:
+                an = False
+                break
+        if an == True:
+            print(i,2*i,3*i)
+        an = True
+
+if __name__ == '__main__':
+    main()
+    pass
+
