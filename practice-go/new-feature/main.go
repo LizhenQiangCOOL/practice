44a6262d22f685d73f3193d2336911073ef6ec28
lzq <798945742@qq.com>
2023-03-29 12:11:45 +0800

    [Update] 写leetcode

--- practice-go/new-feature/main.go
+++ practice-go/new-feature/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
 )
 
 // An IntHeap is a min-heap of ints.
@@ -26,9 +29,38 @@ func (h *IntHeap) Pop() any {
 
 // This example inserts several ints into an IntHeap, checks the minimum,
 // and removes them in order of priority.
+type printContext struct {
+	l  sync.Mutex
+	n  atomic.Value
+	n2 int
+}
+
+func printN(workid int, c *printContext, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		// n := c.n.Load().(int)
+		if c.n2 > 20 {
+			break
+		}
+		c.l.Lock()
+		c.n2 += 1
+		// c.n.Store(n)
+		if c.n2 <= 20 {
+			fmt.Printf("I'm No.%d , print n: %d \n", workid, c.n2)
+		}
+		time.Sleep(time.Microsecond * 30)
+		c.l.Unlock()
+		//runtime.Gosched()
+	}
+}
 func main() {
-	a := "hello中"
-	b := []rune(a)
-	fmt.Printf("%#v\n", b)
-	fmt.Printf("%#v\n", b)
+	c := printContext{l: sync.Mutex{}, n: atomic.Value{}}
+	c.n.Store(0)
+	wg := sync.WaitGroup{}
+	gN := 2
+	for i := 0; i < gN; i++ {
+		wg.Add(1)
+		go printN(i+1, &c, &wg)
+	}
+	wg.Wait()
 }
