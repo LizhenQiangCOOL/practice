189f698acdb9285a0c52b646fdd246485d7094e9
lzqcool <li_zhen_qiang@qq.com>
2023-05-30 17:53:05 +0800

    [Add]增加or-channel例子

---
+++ practice-go/concurrent-programming/or-channel/or_channel.go
@@ -0,0 +1,59 @@
+package or_channel
+
+import (
+	"fmt"
+	"time"
+)
+
+// 利用 or-channel 模式实现一个 or 函数，接收多个 channel，
+// 返回一个 channel，任意一个 channel 有结果后，返回的 channel 都会有结果。
+var or func(channels ...<-chan interface{}) <-chan interface{}
+
+func my() {
+	or = func(channels ...<-chan interface{}) <-chan interface{} {
+		switch len(channels) {
+		case 0:
+			return nil
+		case 1:
+			return channels[0]
+		}
+		orDone := make(chan interface{})
+		go func() {
+			defer close(orDone)
+
+			switch len(channels) {
+			case 2:
+				select {
+				case <-channels[0]:
+				case <-channels[1]:
+				}
+			default:
+				select {
+				case <-channels[0]:
+				case <-channels[1]:
+				case <-channels[2]:
+				case <-or(append(channels[3:], orDone)...):
+				}
+			}
+		}()
+		return orDone
+	}
+
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+	fmt.Printf("done after %v\n", time.Since(start))
+}
