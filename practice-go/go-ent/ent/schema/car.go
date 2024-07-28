0e53ee4bcd548d8eeb8cebe6de7072a92ceb5426
lzq <lizq@qhixp.net>
2024-07-28 23:09:40 +0800

    [Update] 更新信息

---
+++ practice-go/go-ent/ent/schema/car.go
@@ -0,0 +1,18 @@
+package schema
+
+import "entgo.io/ent"
+
+// Car holds the schema definition for the Car entity.
+type Car struct {
+	ent.Schema
+}
+
+// Fields of the Car.
+func (Car) Fields() []ent.Field {
+	return nil
+}
+
+// Edges of the Car.
+func (Car) Edges() []ent.Edge {
+	return nil
+}
---
+++ practice-go/go-ent/start.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"entdemo/ent/user"
+	"fmt"
+	"log"
+
+	"entdemo/ent"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+}
+
+func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	u, err := client.User.
+		Create().
+		SetAge(30).
+		SetName("a8m").
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %w", err)
+	}
+	log.Println("user was created: ", u)
+	return u, nil
+}
+
+func QueryUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	u, err := client.User.
+		Query().
+		Where(user.Name("a8m")).
+		// `Only` fails if no user found,
+		// or more than 1 user returned.
+		Only(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed querying user: %w", err)
+	}
+	log.Println("user returned: ", u)
+	return u, nil
+}
